e5ea0ecd558ed5462f9ec7cff16097fb0d07a6a7
AllenCHM <chengchuanming@hotmail.com>
2015-09-13 01:03:22 +0800

    清理多余测试残留代码

--- parseDict.py
+++ parseDict.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 __author__ = 'AllenCHM'
 
-# def loadText():
-
-# fileName = u'主张词语（中文）.txt'
-# fileName = u'flower.txt'
-fileName = u'正面情感词语（中文）.txt'
 def loadTextDict(fileName):
     f = open(fileName)
-    wordsList = []
+    wordsTuple = []
     for i in f.readlines():
         i = i.strip()
         if i:
-            wordsList.append(i)
+            wordsTuple.append(i)
     f.close()
-    return wordsList
+    return wordsTuple
