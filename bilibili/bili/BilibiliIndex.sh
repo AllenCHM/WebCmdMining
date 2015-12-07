07c666688806af924384242439600fdfb8aabca4
AllenCHM <chengchuanming@hotmail.com>
2015-12-07 21:25:10 +0800

    修改自动脚本scrapy目录

--- bilibili/bili/BilibiliIndex.sh
+++ bilibili/bili/BilibiliIndex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/usr/bin
+export PATH=$PATH:/usr/loccal/bin
 cd /root/WebCmdMining/bilibili/bili
 kill -9 `ps -ef |grep AvIndex|awk '{print $2}' `
 nohup scrapy crawl AvIndex >> ./log/AvIndex.log 2>&1 &
